66c1c810663da5b355c92f7df54d38e9283e24c6
Galih Fitriatmo <galihfitriatmo2611@gmail.com>
2026-02-18 21:42:22 +0700

    update retriever model

--- retriever.py
+++ retriever.py
@@ -1,30 +1,45 @@
 import os
 from dotenv import load_dotenv
-from langchain.vectorstores import FAISS
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.schema import Document
 from tools import df, build_block
 
 load_dotenv()
 
 INDEX_DIR = "database/faiss_recipes_index"
-CSV_PATH = "database/df_resep_cleaned.csv"
 
-embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+embedding_model = HuggingFaceEmbeddings(
+    model_name="all-MiniLM-L6-v2"
+)
 
-def load_retriever() -> FAISS:
+def load_retriever():
+
+    # Kalau index sudah ada → load
     if os.path.exists(INDEX_DIR):
-        vs = FAISS.load_local(INDEX_DIR, embedding_model, allow_dangerous_deserialization=True)
+        print("Loading FAISS index...")
+        vs = FAISS.load_local(
+            INDEX_DIR,
+            embedding_model,
+            allow_dangerous_deserialization=True
+        )
+
     else:
+        print("Membuat FAISS index (11k dokumen)...")
+
         docs = [
             Document(
                 page_content=build_block(r),
-                metadata={"loves": int(r.loves)},
+                metadata={"loves": int(r.loves)}  # pastikan huruf besar
             )
             for r in df.itertuples(index=False)
         ]
+
         vs = FAISS.from_documents(docs, embedding_model)
         vs.save_local(INDEX_DIR)
+
+        print("FAISS selesai dibuat!")
+
     return vs.as_retriever(search_kwargs={"k": 4})
 
 
